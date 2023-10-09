9404391a41f05c3c27230ff0384cfbfee075478f
minizzang <minizzang@kaist.ac.kr>
2023-10-09 23:30:56 +0900

    Draw current time on canvas

--- wear/src/main/java/com/example/wear_study/watchface/DualCanvasRenderer.kt
+++ wear/src/main/java/com/example/wear_study/watchface/DualCanvasRenderer.kt
@@ -1,7 +1,11 @@
 package com.example.wear_study.watchface
 
+import android.content.ContentValues.TAG
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.graphics.Rect
+import android.util.Log
 import android.view.SurfaceHolder
 import androidx.wear.watchface.Renderer
 import androidx.wear.watchface.WatchState
@@ -39,12 +43,28 @@ class DualCanvasRenderer(
         zonedDateTime: ZonedDateTime,
         sharedAssets: DualAssets
     ) {
+        val h_width = bounds.width() / 2f
+        val h_height = bounds.height() / 2f
+        val p_black = Paint()
+
+        canvas.drawColor(Color.WHITE)
+
+        p_black.color = Color.BLACK
+        p_black.textAlign = Paint.Align.CENTER
+        p_black.textSize = 100f
+        p_black.strokeWidth = 40f
+
+        val timeText = zonedDateTime.hour.toString() + ":" + zonedDateTime.minute.toString()
+        val textBounds = Rect()
+        p_black.getTextBounds(timeText, 0, timeText.length, textBounds)
+        canvas.drawText(timeText, h_width, h_height-textBounds.exactCenterY(), p_black)
     }
+
 }
 
 class DualAssets: Renderer.SharedAssets {
     override fun onDestroy() {
-        TODO("Not yet implemented")
+
     }
 
 }
